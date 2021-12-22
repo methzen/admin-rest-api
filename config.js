fd162b6784bf80f13c683b615349c43e922060dc
methzen <diamohaminsa@gmail.com>
2021-12-22 22:03:22 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    devAdminURL: "http://localhost:3001",
+    prodAdminURL: "https://www.admin.mouhamadoudia.fr",
+    devFrontendWebsiteURL: "http://localhost:3000",
+    prodFrontendWebsiteURL: "https://www.mouhamadoudia.fr"
+  }
+  
+  module.exports = config
